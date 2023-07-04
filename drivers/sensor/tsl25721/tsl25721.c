7a753f25bba2b69161dbdf78f462d47a3705d4de
Kotaro SHIOTA <shiota.kotaro@fujitsu.com>
2023-07-04 16:40:02 +0900

    AAA

--- drivers/sensor/tsl25721/tsl25721.c
+++ drivers/sensor/tsl25721/tsl25721.c
@@ -56,7 +56,7 @@ static int vcnl4040_sample_fetch(const struct device *dev,
 	struct vcnl4040_data *data = dev->data;
 	int ret = 0;
 
-#ifdef CONFIG_VCNL4040_ENABLE_ALS
+#ifdef CONFIG_TSL25721_ENABLE_ALS
 	__ASSERT_NO_MSG(chan == SENSOR_CHAN_ALL ||
 			chan == SENSOR_CHAN_PROX ||
 			chan == SENSOR_CHAN_LIGHT);
@@ -66,16 +66,16 @@ static int vcnl4040_sample_fetch(const struct device *dev,
 	k_mutex_lock(&data->mutex, K_FOREVER);
 
 	if (chan == SENSOR_CHAN_ALL || chan == SENSOR_CHAN_PROX) {
-		ret = vcnl4040_read(dev, VCNL4040_REG_PS_DATA,
+		ret = vcnl4040_read(dev, TSL25721_REG_PS_DATA,
 				    &data->proximity);
 		if (ret < 0) {
 			LOG_ERR("Could not fetch proximity");
 			goto exit;
 		}
 	}
-#ifdef CONFIG_VCNL4040_ENABLE_ALS
+#ifdef CONFIG_TSL25721_ENABLE_ALS
 	if (chan == SENSOR_CHAN_ALL || chan == SENSOR_CHAN_LIGHT) {
-		ret = vcnl4040_read(dev, VCNL4040_REG_ALS_DATA,
+		ret = vcnl4040_read(dev, TSL25721_REG_ALS_DATA,
 				    &data->light);
 		if (ret < 0) {
 			LOG_ERR("Could not fetch ambient light");
@@ -103,7 +103,7 @@ static int vcnl4040_channel_get(const struct device *dev,
 		val->val2 = 0;
 		break;
 
-#ifdef CONFIG_VCNL4040_ENABLE_ALS
+#ifdef CONFIG_TSL25721_ENABLE_ALS
 	case SENSOR_CHAN_LIGHT:
 		val->val1 = data->light * data->sensitivity;
 		val->val2 = 0;
@@ -122,34 +122,34 @@ static int vcnl4040_channel_get(const struct device *dev,
 static int vcnl4040_reg_setup(const struct device *dev)
 {
 	const struct vcnl4040_config *config = dev->config;
-	uint16_t value[VCNL4040_RW_REG_COUNT] = { 0 };
+	uint16_t value[TSL25721_RW_REG_COUNT] = { 0 };
 	uint8_t reg;
 	int ret = 0;
 
-#ifdef CONFIG_VCNL4040_ENABLE_ALS
+#ifdef CONFIG_TSL25721_ENABLE_ALS
 	struct vcnl4040_data *data = dev->data;
 
 	/* Set ALS integration time */
-	value[VCNL4040_REG_ALS_CONF] = config->als_it << VCNL4040_ALS_IT_POS;
+	value[TSL25721_REG_ALS_CONF] = config->als_it << TSL25721_ALS_IT_POS;
 	/* Clear ALS shutdown */
-	value[VCNL4040_REG_ALS_CONF] &= ~VCNL4040_ALS_SD_MASK;
+	value[TSL25721_REG_ALS_CONF] &= ~TSL25721_ALS_SD_MASK;
 
 	/*
 	 * scale the lux depending on the value of the integration time
-	 * see page 8 of the VCNL4040 application note:
+	 * see page 8 of the TSL25721 application note:
 	 * https://www.vishay.com/docs/84307/designingvcnl4040.pdf
 	 */
 	switch (config->als_it) {
-	case VCNL4040_AMBIENT_INTEGRATION_TIME_80MS:
+	case TSL25721_AMBIENT_INTEGRATION_TIME_80MS:
 		data->sensitivity = 0.12;
 		break;
-	case VCNL4040_AMBIENT_INTEGRATION_TIME_160MS:
+	case TSL25721_AMBIENT_INTEGRATION_TIME_160MS:
 		data->sensitivity = 0.06;
 		break;
-	case VCNL4040_AMBIENT_INTEGRATION_TIME_320MS:
+	case TSL25721_AMBIENT_INTEGRATION_TIME_320MS:
 		data->sensitivity = 0.03;
 		break;
-	case VCNL4040_AMBIENT_INTEGRATION_TIME_640MS:
+	case TSL25721_AMBIENT_INTEGRATION_TIME_640MS:
 		data->sensitivity = 0.015;
 		break;
 	default:
@@ -159,20 +159,20 @@ static int vcnl4040_reg_setup(const struct device *dev)
 		break;
 	}
 #else
-	value[VCNL4040_REG_ALS_CONF] = VCNL4040_ALS_SD_MASK; /* default */
+	value[TSL25721_REG_ALS_CONF] = TSL25721_ALS_SD_MASK; /* default */
 #endif
 
 	/* Set PS_HD */
-	value[VCNL4040_REG_PS_CONF] = VCNL4040_PS_HD_MASK;
+	value[TSL25721_REG_PS_CONF] = TSL25721_PS_HD_MASK;
 	/* Set duty cycle */
-	value[VCNL4040_REG_PS_CONF] |= config->led_dc << VCNL4040_PS_DUTY_POS;
+	value[TSL25721_REG_PS_CONF] |= config->led_dc << TSL25721_PS_DUTY_POS;
 	/* Set integration time */
-	value[VCNL4040_REG_PS_CONF] |= config->proxy_it << VCNL4040_PS_IT_POS;
+	value[TSL25721_REG_PS_CONF] |= config->proxy_it << TSL25721_PS_IT_POS;
 	/* Clear proximity shutdown */
-	value[VCNL4040_REG_PS_CONF] &= ~VCNL4040_PS_SD_MASK;
+	value[TSL25721_REG_PS_CONF] &= ~TSL25721_PS_SD_MASK;
 
 	/* Set LED current */
-	value[VCNL4040_REG_PS_MS] = config->led_i << VCNL4040_LED_I_POS;
+	value[TSL25721_REG_PS_MS] = config->led_i << TSL25721_LED_I_POS;
 
 	for (reg = 0; reg < ARRAY_SIZE(value); reg++) {
 		ret |= vcnl4040_write(dev, reg, value[reg]);
@@ -188,30 +188,30 @@ static int vcnl4040_pm_action(const struct device *dev,
 	int ret = 0;
 	uint16_t ps_conf;
 
-	ret = vcnl4040_read(dev, VCNL4040_REG_PS_CONF, &ps_conf);
+	ret = vcnl4040_read(dev, TSL25721_REG_PS_CONF, &ps_conf);
 	if (ret < 0)
 		return ret;
-#ifdef CONFIG_VCNL4040_ENABLE_ALS
+#ifdef CONFIG_TSL25721_ENABLE_ALS
 	uint16_t als_conf;
 
-	ret = vcnl4040_read(dev, VCNL4040_REG_ALS_CONF, &als_conf);
+	ret = vcnl4040_read(dev, TSL25721_REG_ALS_CONF, &als_conf);
 	if (ret < 0)
 		return ret;
 #endif
 	switch (action) {
 	case PM_DEVICE_ACTION_RESUME:
 		/* Clear proximity shutdown */
-		ps_conf &= ~VCNL4040_PS_SD_MASK;
+		ps_conf &= ~TSL25721_PS_SD_MASK;
 
-		ret = vcnl4040_write(dev, VCNL4040_REG_PS_CONF,
+		ret = vcnl4040_write(dev, TSL25721_REG_PS_CONF,
 					ps_conf);
 		if (ret < 0)
 			return ret;
-#ifdef CONFIG_VCNL4040_ENABLE_ALS
+#ifdef CONFIG_TSL25721_ENABLE_ALS
 		/* Clear als shutdown */
-		als_conf &= ~VCNL4040_ALS_SD_MASK;
+		als_conf &= ~TSL25721_ALS_SD_MASK;
 
-		ret = vcnl4040_write(dev, VCNL4040_REG_ALS_CONF,
+		ret = vcnl4040_write(dev, TSL25721_REG_ALS_CONF,
 					als_conf);
 		if (ret < 0)
 			return ret;
@@ -219,17 +219,17 @@ static int vcnl4040_pm_action(const struct device *dev,
 		break;
 	case PM_DEVICE_ACTION_SUSPEND:
 		/* Set proximity shutdown bit 0 */
-		ps_conf |= VCNL4040_PS_SD_MASK;
+		ps_conf |= TSL25721_PS_SD_MASK;
 
-		ret = vcnl4040_write(dev, VCNL4040_REG_PS_CONF,
+		ret = vcnl4040_write(dev, TSL25721_REG_PS_CONF,
 					ps_conf);
 		if (ret < 0)
 			return ret;
-#ifdef CONFIG_VCNL4040_ENABLE_ALS
+#ifdef CONFIG_TSL25721_ENABLE_ALS
 		/* Clear als shutdown bit 0 */
-		als_conf |= VCNL4040_ALS_SD_MASK;
+		als_conf |= TSL25721_ALS_SD_MASK;
 
-		ret = vcnl4040_write(dev, VCNL4040_REG_ALS_CONF,
+		ret = vcnl4040_write(dev, TSL25721_REG_ALS_CONF,
 					als_conf);
 		if (ret < 0)
 			return ret;
@@ -256,12 +256,12 @@ static int vcnl4040_init(const struct device *dev)
 	}
 
 	/* Check device id */
-	if (vcnl4040_read(dev, VCNL4040_REG_DEVICE_ID, &id)) {
+	if (vcnl4040_read(dev, TSL25721_REG_DEVICE_ID, &id)) {
 		LOG_ERR("Could not read device id");
 		return -EIO;
 	}
 
-	if (id != VCNL4040_DEFAULT_ID) {
+	if (id != TSL25721_DEFAULT_ID) {
 		LOG_ERR("Incorrect device id (%d)", id);
 		return -EIO;
 	}
@@ -273,7 +273,7 @@ static int vcnl4040_init(const struct device *dev)
 
 	k_mutex_init(&data->mutex);
 
-#if CONFIG_VCNL4040_TRIGGER
+#if CONFIG_TSL25721_TRIGGER
 	if (vcnl4040_trigger_init(dev)) {
 		LOG_ERR("Could not initialise interrupts");
 		return -EIO;
@@ -288,18 +288,18 @@ static int vcnl4040_init(const struct device *dev)
 static const struct sensor_driver_api vcnl4040_driver_api = {
 	.sample_fetch = vcnl4040_sample_fetch,
 	.channel_get = vcnl4040_channel_get,
-#ifdef CONFIG_VCNL4040_TRIGGER
+#ifdef CONFIG_TSL25721_TRIGGER
 	.attr_set = vcnl4040_attr_set,
 	.trigger_set = vcnl4040_trigger_set,
 #endif
 };
 
-#define VCNL4040_DEFINE(inst)									\
+#define TSL25721_DEFINE(inst)									\
 	static struct vcnl4040_data vcnl4040_data_##inst;					\
 												\
 	static const struct vcnl4040_config vcnl4040_config_##inst = {				\
 		.i2c = I2C_DT_SPEC_INST_GET(inst),						\
-		IF_ENABLED(CONFIG_VCNL4040_TRIGGER,						\
+		IF_ENABLED(CONFIG_TSL25721_TRIGGER,						\
 			   (.int_gpio = GPIO_DT_SPEC_INST_GET_OR(inst, int_gpios, { 0 }),))	\
 		.led_i = DT_INST_ENUM_IDX(inst, led_current),					\
 		.led_dc = DT_INST_ENUM_IDX(inst, led_duty_cycle),				\
@@ -314,4 +314,4 @@ static const struct sensor_driver_api vcnl4040_driver_api = {
 			      &vcnl4040_data_##inst, &vcnl4040_config_##inst, POST_KERNEL,	\
 			      CONFIG_SENSOR_INIT_PRIORITY, &vcnl4040_driver_api);		\
 
-DT_INST_FOREACH_STATUS_OKAY(VCNL4040_DEFINE)
+DT_INST_FOREACH_STATUS_OKAY(TSL25721_DEFINE)
